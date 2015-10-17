990b9a3f328443753cc590ac407177e7977ab419
Kyle Espinola <ksespinola@gmail.com>
2015-10-17 11:41:06 -0700

    work with autoValue for user full_name

--- packages/user/both/collection.js
+++ packages/user/both/collection.js
@@ -2,35 +2,102 @@ const {
   compose,
 } = R;
 
-function generateFullName(doc){
-  if(!doc) return doc;
-
-  let {
-    profile,
-  } = doc;
-
-  if(!profile) return doc;
-
-  const {
-    first_name,
-    last_name,
-  } = profile;
-
-  doc.full_name = `${first_name}${last_name ? ` ${last_name}` : ''}`;
-
-  return doc;
+function getFullName({first_name, last_name}){
+  if(!first_name) return '';
+  
+  return `${first_name}${last_name ? ` ${last_name}` : ''}`;
+  
 }
 
-Meteor.users.after.insert((userId, doc) => {
-  compose(
-    generateFullName
-  )(doc);
+Schema = {};
+
+Schema.UserCountry = new SimpleSchema({
+  name: {
+    type: String
+  },
+  code: {
+    type: String,
+    regEx: /^[A-Z]{2}$/
+  }
 });
 
-Meteor.users.after.update((userId, doc) => {
-  compose(
-    generateFullName
-  )(doc);
-  console.log(doc, 'after update');
+Schema.UserProfile = new SimpleSchema({
+  full_name: {
+    type: String,
+    autoValue: () => {
+      console.log(this.fields('profile'), 'autoValue full_name', getFullName(this.fields('profile')));
+      if (this.isInsert) {
+        return getFullName(this.field('profile'));
+      } else if (this.isUpsert) {
+        return {$setOnInsert: getFullName(this.field('profile'))};
+      }
+    },
+    optional: true,
+  },
+  first_name: {
+    type: String,
+    optional: true
+  },
+  last_name: {
+    type: String,
+    optional: true
+  },
+  birthday: {
+    type: Date,
+    optional: true
+  },
+  website: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Url,
+    optional: true
+  },
+  bio: {
+    type: String,
+    optional: true
+  },
+  country: {
+    type: Schema.UserCountry,
+    optional: true
+  }
 });
+
+Schema.User = new SimpleSchema({
+  username: {
+    type: String,
+    optional: true
+  },
+  emails: {
+    type: Array,
+    optional: true
+  },
+  "emails.$": {
+    type: Object
+  },
+  "emails.$.address": {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  "emails.$.verified": {
+    type: Boolean
+  },
+  createdAt: {
+    type: Date
+  },
+  profile: {
+    type: Schema.UserProfile,
+    optional: true
+  },
+  services: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+  roles: {
+    type: Object,
+    optional: true,
+    blackbox: true
+  },
+});
+
+Meteor.users.attachSchema(Schema.User);
 
